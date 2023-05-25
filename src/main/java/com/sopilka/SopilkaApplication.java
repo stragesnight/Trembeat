a7925de8e7a6cc46814f6b520023e63853cb291d
stragesnight <alex.spresyo.o@gmail.com>
2023-05-25 19:04:18 +0300

    Add default project files

---
+++ src/main/java/com/sopilka/SopilkaApplication.java
@@ -0,0 +1,13 @@
+package com.sopilka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopilkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopilkaApplication.class, args);
+    }
+
+}
