d42563bef1916524046ef8a0398a9cbdef5a6007
stragesnight <alex.spresyo.o@gmail.com>
2023-06-14 19:23:02 +0300

    Move UserService methods to UserRestController

--- src/main/java/com/trembeat/controllers/UserRestController.java
+++ src/main/java/com/trembeat/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package com.trembeat.controllers;
 
 import com.trembeat.domain.models.*;
+import com.trembeat.domain.repository.*;
 import com.trembeat.domain.viewmodels.*;
 import com.trembeat.services.*;
 import jakarta.validation.Valid;
@@ -8,16 +9,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.*;
 
 @Controller
 public class UserRestController extends GenericContentController {
     @Autowired
-    private UserService _userService;
+    private UserRepository _userRepo;
+    @Autowired
+    private ImageRepository _imageRepo;
+    @Autowired
+    private ImageStorageService _storageService;
+    @Autowired
+    private RoleRepository _roleRepo;
+    private BCryptPasswordEncoder _passwordEncoder;
 
+    private Role _roleUser = null;
+    private Image _defaultPicture = null;
+
+
+
+    public UserRestController() {
+        _passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     @GetMapping("/api/get-profile-picture")
     public ResponseEntity<?> getProfilePicture(@RequestParam("id") Long id) {
@@ -26,19 +44,35 @@ public class UserRestController extends GenericContentController {
 
     @GetMapping("/api/get-user")
     public ResponseEntity<?> getUser(@RequestParam("id") Long id) {
-        User user = _userService.findById(id);
-        if (user == null)
+        Optional<User> optionalUser = _userRepo.findById(id);
+        if (optionalUser.isEmpty())
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        return new ResponseEntity<>(new UserViewModel(user), null, HttpStatus.OK);
+        return new ResponseEntity<>(new UserViewModel(optionalUser.get()), null, HttpStatus.OK);
     }
 
     @PostMapping("/api/put-user")
     public ResponseEntity<?> putUser(@ModelAttribute("user") @Valid UserRegisterViewModel viewModel) {
-        if (_userService.registerUser(viewModel) == null)
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        User user = new User(
+                viewModel.getUsername(),
+                "",
+                _passwordEncoder.encode(viewModel.getPassword()),
+                viewModel.getEmail());
 
-        User user = (User)_userService.loadUserByUsername(viewModel.getUsername());
+        if (_roleUser == null)
+            _roleUser = _roleRepo.findByName("ROLE_USER").get();
+        if (_defaultPicture == null)
+            _defaultPicture = _imageRepo.findById(1L).get();
+
+        user.addAuthority(_roleUser);
+        user.setProfilePicture(_defaultPicture);
+
+        try {
+            _userRepo.save(user);
+        } catch (Exception ex) {
+            return null;
+        }
+
         return getProfileRedirect(user);
     }
 
@@ -48,15 +82,41 @@ public class UserRestController extends GenericContentController {
             Authentication auth,
             @ModelAttribute("user") @Valid UserEditViewModel viewModel) {
 
+        Map<String, String> errors = new HashMap<>();
         User user = (User)auth.getPrincipal();
         if (user == null)
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        User updatedUser = _userService.updateUser(user.getId(), viewModel);
-        if (updatedUser == null)
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        user.setUsername(viewModel.getUsername());
+        user.setBio(viewModel.getBio());
 
-        user.copyFrom(updatedUser);
+        if (viewModel.getPassword() != null && !viewModel.getPassword().isEmpty()) {
+            user.setPassword(_passwordEncoder.encode(viewModel.getPassword()));
+            user.setEmail(viewModel.getEmail());
+        }
+
+        if (viewModel.getProfilePicture() != null) {
+            Image picture = new Image();
+            picture.setMimeType(viewModel.getProfilePicture().getContentType());
+            if (!_storageService.isAcceptedContentType(picture.getMimeType()))
+                errors.put("profilePicture", "error.unaccepted_content_type");
+
+            try {
+                picture = _imageRepo.save(picture);
+                _storageService.save(picture, viewModel.getProfilePicture().getInputStream());
+                picture = _imageRepo.save(picture);
+                user.setProfilePicture(picture);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            user = _userRepo.save(user);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
 
         return getProfileRedirect(user);
     }
@@ -68,8 +128,12 @@ public class UserRestController extends GenericContentController {
         if (user == null)
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        if (!_userService.deleteUser(user.getId()))
+        try {
+            _storageService.delete(user.getProfilePicture());
+            _userRepo.delete(user);
+        } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
         return new ResponseEntity<>(
                 new Response(null, "/"),
--- src/main/java/com/trembeat/services/UserService.java
+++ src/main/java/com/trembeat/services/UserService.java
@@ -1,12 +1,8 @@
 package com.trembeat.services;
 
-import com.trembeat.domain.models.*;
-import com.trembeat.domain.models.User;
 import com.trembeat.domain.repository.*;
-import com.trembeat.domain.viewmodels.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -17,21 +13,7 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository _userRepo;
-    @Autowired
-    private ImageRepository _imageRepo;
-    @Autowired
-    private ImageStorageService _storageService;
-    @Autowired
-    private RoleRepository _roleRepo;
-    private BCryptPasswordEncoder _passwordEncoder;
 
-    private Role _roleUser = null;
-    private Image _defaultPicture = null;
-
-
-    public UserService() {
-        _passwordEncoder = new BCryptPasswordEncoder();
-    }
 
     /**
      * Find user by username
@@ -43,96 +25,5 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return _userRepo.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("Username not found"));
-    }
-
-    /**
-     * Find user by id
-     * @param id Id to search for
-     * @return Found User entity
-     */
-    public User findById(Long id) {
-        return _userRepo.findById(id).orElse(null);
-    }
-
-    /**
-     * Register new user
-     * @param viewModel User data to be registered
-     * @return true, if user was registered successfully, otherwise - false
-     */
-    public User registerUser(UserRegisterViewModel viewModel) {
-        try {
-            User user = new User(
-                    viewModel.getUsername(),
-                    "",
-                    _passwordEncoder.encode(viewModel.getPassword()),
-                    viewModel.getEmail());
-
-            if (_roleUser == null)
-                _roleUser = _roleRepo.findByName("ROLE_USER").get();
-            if (_defaultPicture == null)
-                _defaultPicture = _imageRepo.findById(1L).get();
-
-            user.addAuthority(_roleUser);
-            user.setProfilePicture(_defaultPicture);
-
-            _userRepo.save(user);
-
-            return user;
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
-    /**
-     * Update existing user
-     * @param userId Existing user id to be updated
-     * @param viewModel Updated user date
-     * @return true, if user was updated successfully, otherwise - false
-     */
-    public User updateUser(Long userId, UserEditViewModel viewModel) {
-        try {
-            User user = _userRepo.findById(userId).get();
-            user.setUsername(viewModel.getUsername());
-            user.setBio(viewModel.getBio());
-
-            if (viewModel.getPassword() != null && !viewModel.getPassword().isEmpty()) {
-                user.setPassword(_passwordEncoder.encode(viewModel.getPassword()));
-                user.setEmail(viewModel.getEmail());
-            }
-
-            if (viewModel.getProfilePicture() != null) {
-                Image picture = new Image();
-                picture.setMimeType(viewModel.getProfilePicture().getContentType());
-                if (!_storageService.isAcceptedContentType(picture.getMimeType()))
-                    return null;
-
-                picture = _imageRepo.save(picture);
-                _storageService.save(picture, viewModel.getProfilePicture().getInputStream());
-                picture = _imageRepo.save(picture);
-                user.setProfilePicture(picture);
-            }
-
-            user = _userRepo.save(user);
-            return user;
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-            return null;
-        }
-    }
-
-    /**
-     * Delete user from database
-     * @param userId Id of user to be deleted
-     * @return true, if user was deleted successfully, otherwise - false
-     */
-    public boolean deleteUser(Long userId) {
-        try {
-            User user = _userRepo.findById(userId).get();
-            _storageService.delete(user.getProfilePicture());
-            _userRepo.delete(user);
-            return true;
-        } catch (Exception ex) {
-            return false;
-        }
     }
 }
